dcde1ec928cfab5c1c74995596deb18833c0aab5
Антон Куканов <motokyka@yandex.ru>
2024-12-20 00:58:19 +0300

    добавлен метод list

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,23 +1,23 @@
 import random
+import re
 import string
+
 from django.core.mail import send_mail
-from rest_framework import status, views, viewsets, permissions, generics
+from rest_framework import status, viewsets, permissions, generics, filters
 from rest_framework.decorators import action
-from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework_simplejwt import serializers
 from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from users.models import User
 from users.serializers import (
-    UserRegistrationSerializer,
     UserSerializer,
-    ChangePasswordSerializer, UserTokenSerializer,
+    UserTokenSerializer,
+    UserRegistrationSerializer, SignUpSerializer,
 )
 
 
@@ -29,16 +29,19 @@ class UserMeView(generics.RetrieveAPIView):
         return self.request.user
 
     def patch(self, request):
+        username = request.data.get('username', None)
+
+        if username:
+            if not re.match(r"^[\w.@+-]+\Z", username):
+                return Response({"error": "Никнейм содержит недопустимы символы!"},
+                                status=status.HTTP_400_BAD_REQUEST)
+
         serializer = UserSerializer(request.user, data=request.data, partial=True)
+
         if serializer.is_valid():
-            try:
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            except Exception as e:
-                return Response(
-                    {"error": str(e)},
-                    status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                )
+            serializer.save()
+            return Response(serializer.data)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -46,7 +49,9 @@ class UserViewSet(viewsets.ModelViewSet):
     lookup_field = "username"
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ("username",)
     pagination_class = PageNumberPagination
 
     def get_permissions(self):
@@ -54,12 +59,23 @@ class UserViewSet(viewsets.ModelViewSet):
             return [permissions.AllowAny()]
         elif self.action in ['current_user', 'partial_update', 'user_delete']:
             return [permissions.IsAuthenticated()]
+        elif self.action == 'list':
+            return [permissions.IsAuthenticated()]
         return [permissions.IsAdminUser()]
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        serializer = self.get_serializer(page, many=True)
+        return self.get_paginated_response(serializer.data)
 
     def create(self, request, *args, **kwargs):
 
         serializer = UserRegistrationSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+
+        if not serializer.is_valid():
+            errors = serializer.errors
+            return Response(errors, status=status.HTTP_400_BAD_REQUEST)
 
         role = serializer.validated_data.pop("role", "user")
         email = serializer.validated_data["email"]
@@ -108,28 +124,28 @@ class UserViewSet(viewsets.ModelViewSet):
             status=status.HTTP_200_OK,
         )
 
-    def update(self, request, *args, **kwargs):
-        user = get_object_or_404(User, username=self.kwargs["username"])
-        serializer = ChangePasswordSerializer(data=request.data)
-
-        if serializer.is_valid():
-            if not user.check_password(serializer.validated_data["old_password"]):
-                return Response(
-                    {"old_password": ["Неправильный пароль."]},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-            user.set_password(serializer.validated_data["new_password"])
-            user.save()
-            return Response(
-                {
-                    "status": "success",
-                    "code": status.HTTP_200_OK,
-                    "message": "Пароль обновлен успешно",
-                    "data": [],
-                },
-                status=status.HTTP_200_OK,
-            )
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def update(self, request, *args, **kwargs):
+    #     user = get_object_or_404(User, username=self.kwargs["username"])
+    #     serializer = ChangePasswordSerializer(data=request.data)
+    #
+    #     if serializer.is_valid():
+    #         if not user.check_password(serializer.validated_data["old_password"]):
+    #             return Response(
+    #                 {"old_password": ["Неправильный пароль."]},
+    #                 status=status.HTTP_400_BAD_REQUEST,
+    #             )
+    #         user.set_password(serializer.validated_data["new_password"])
+    #         user.save()
+    #         return Response(
+    #             {
+    #                 "status": "success",
+    #                 "code": status.HTTP_200_OK,
+    #                 "message": "Пароль обновлен успешно",
+    #                 "data": [],
+    #             },
+    #             status=status.HTTP_200_OK,
+    #         )
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @staticmethod
     def send_confirmation_email(user, confirmation_code):
@@ -156,21 +172,36 @@ class UserViewSet(viewsets.ModelViewSet):
             return Response(serializer.data)
         return Response("Unauthorized", status=status.HTTP_401_UNAUTHORIZED)
 
-    @action(detail=False)
-    def user_delete(self, request, username):
-        user = get_object_or_404(User, username=username)
-        if request.user.is_admin or request.user.is_superuser:
-            user.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        else:
-            return Response(status=status.HTTP_403_FORBIDDEN)
+    @action(detail=False, methods=['delete'])
+    def user_delete(self, request):
+        user = request.user
+        user.delete()
+        return Response("User deleted successfully",
+                        status=status.HTTP_200_OK)
 
     def get_queryset(self):
         queryset = super().get_queryset()
         username = self.request.query_params.get("search")
         if username is not None:
-            queryset = queryset.filter(username__icontains=username).distinct()
+            queryset = queryset.filter(username=username).distinct()
         return queryset
+
+
+class SignUpView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = SignUpSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = User.objects.create_user(**serializer.validated_data)
+            user.is_active = False
+            user.save()
+            return Response(serializer.data,
+                            status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
@@ -186,10 +217,11 @@ class CustomTokenObtainPairView(TokenObtainPairView):
             data = {
                 "token": str(token.access_token),
             }
+            return Response(data, status=status.HTTP_200_OK)
+
         except TokenError as e:
             raise InvalidToken(e.args[0])
-        except User.DoesNotExist:
-            raise serializers.ValidationError("Пользователь не найден",
-                                              code=status.HTTP_404_NOT_FOUND)
 
-        return Response(data, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response("Пользователь не найден",
+                            status=status.HTTP_404_NOT_FOUND)
