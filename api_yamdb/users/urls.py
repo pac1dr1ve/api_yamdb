78dacb56d6e76f6230b48e2b89162a48d6ca82df
Антон Куканов <motokyka@yandex.ru>
2025-01-04 10:31:50 +0300

    убран закомментированный код

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -3,18 +3,6 @@ from rest_framework.routers import DefaultRouter
 
 from users.views import CustomTokenObtainPairView, UserMeView, UserViewSet, SignUpView
 
-# v1_router.register('users', UserViewSet, basename='users')
-# urlpatterns = [
-#     path("auth/signup/", UserViewSet.as_view({"post": "create"}), name="auth_signup"),
-#     path("auth/token/", CustomTokenObtainPairView.as_view()),
-#     path("users/me/", UserMeView.as_view(), name="users_me"),
-#     path("users/<str:username>/", UserViewSet.as_view({"get": "retrieve",
-#                                                        "delete": "destroy"}), name="user_detail"),
-#     path("users/<int:pk>/", UserViewSet.as_view({"get": "retrieve",
-#                                                  "delete": "destroy"}), name="user"),
-#     path("users/set_password/", UserViewSet.as_view({"post": "update"}), name="user_update"),
-#     path("users/current_user/", UserMeView.as_view(), name="current_user"),
-# ]
 v1_router = DefaultRouter()
 v1_router.register(r"users", UserViewSet, basename='user')
 
@@ -24,5 +12,4 @@ urlpatterns = [
     path("auth/signup/", SignUpView.as_view({"post": "create"}), name="signup"),
     path("auth/token/", CustomTokenObtainPairView.as_view(), name="token"),
     path("users/me/", UserMeView.as_view(), name="users_me"),
-    # path("users/<str:username>/", UserViewSet.as_view({"put": "update"}), name="user_detail"),
 ]
