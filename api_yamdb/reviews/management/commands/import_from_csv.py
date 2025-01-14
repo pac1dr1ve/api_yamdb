60a852b8478c454de1d3a82a3ed672098513619f
Alexander Boyko <sasha.boyko.dg.19@gmail.com>
2025-01-14 14:50:30 +0300

    автоматизировал скрипт для загрузки данных

--- api_yamdb/reviews/management/commands/import_from_csv.py
+++ api_yamdb/reviews/management/commands/import_from_csv.py
@@ -1,3 +1,4 @@
+import os
 import csv
 
 from django.core.management.base import BaseCommand
@@ -7,64 +8,91 @@ from users.models import User
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        parser.add_arguments('csv_file', nargs='+', type=str)
 
-    def handle(self, *args, **options):
-        for csv_file in options['csv_file']:
-            reader = csv.DictReader(
-                open(
-                    csv_file, newline='', encoding='utf-8'
-                )
-            )
-            for row in reader:
-                if 'category.csv' in csv_file:
-                    Category.objects.create(
-                        id=row['id'], name=row['name'], slug=row['slug']
-                    )
-                if 'comments.csv' in csv_file:
-                    Comment.objects.create(
-                        id=row['id'],
-                        review_id=row['review_id'],
-                        text=row['text'],
-                        author=row['author'],
-                        pub_date=row['pub_date']
-                    )
-                if 'genre_title.csv' in csv_file:
-                    GenreTitle.objects.create(
-                        id=row['id'],
-                        title_id=row['title_id'],
-                        genre_id=row['genre_id']
-                    )
-                if 'genre.csv' in csv_file:
-                    Genre.objects.create(
-                        id=row['id'],
-                        name=row['name'],
-                        slug=row['slug']
-                    )
-                if 'review.csv' in csv_file:
-                    Review.objects.create(
-                        id=row['id'],
-                        title_id=row['title_id'],
-                        text=row['text'],
-                        author=row['author'],
-                        score=row['score'],
-                        pub_date=row['pub_date']
-                    )
-                if 'title.csv' in csv_file:
-                    Title.objects.create(
-                        id=row['id'],
-                        name=row['name'],
-                        year=row['year'],
-                        category=row['category']
-                    )
-                if 'users.csv' in csv_file:
-                    User.objects.create(
-                        id=row['id'],
-                        username=row['username'],
-                        email=row['email'],
-                        role=row['role'],
-                        bio=row['bio'],
-                        first_name=row['first_name'],
-                        last_name=row['last_name']
-                    )
+    def handle(self, *args, **kwargs):
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        data_dir = os.path.join(base_dir, 'data')
+
+        if not os.path.exists(data_dir):
+            self.stdout.write(self.style.ERROR(f'Папка с данными не найдена: {data_dir}'))
+            return
+
+        file_to_model = {
+            'category.csv': Category,
+            'comments.csv': Comment,
+            'titles.csv': Title,
+            'genre.csv': Genre,
+            'genre_title.csv': GenreTitle,
+            'review.csv': Review,
+            'users.csv': User,
+        }
+
+        for file, model in file_to_model.items():
+            file_path = os.path.join(data_dir, file)
+            if not os.path.exists(file_path):
+                self.stdout.write(self.style.WARNING(f'Файл {file} не найден, продолжаем'))
+                continue
+            self.stdout.write(self.style.SUCCESS(f'Файл {file} загружен.'))
+
+            with open(file_path, newline='', encoding='utf-8') as csvfile:
+                reader = csv.DictReader(csvfile)
+                objects = []
+
+                for row in reader:
+                    if model == Category:
+                        objects.append(Category(
+                            id=row['id'],
+                            name=row['name'],
+                            slug=row['slug']
+                        ))
+                    if model == Comment:
+                        objects.append(Comment(
+                            id=row['id'],
+                            review_id=row['review_id'],
+                            text=row['text'],
+                            author=User.objects.get(id=row['author']),
+                            pub_date=row['pub_date']
+                        ))
+                    if model == Title:
+                        objects.append(Title(
+                            id=row['id'],
+                            name=row['name'],
+                            year=row['year'],
+                            category=Category.objects.get(id=row['category'])
+                        ))
+                    if model == Genre:
+                        objects.append(Genre(
+                            id=row['id'],
+                            name=row['name'],
+                            slug=row['slug']
+                        ))
+                    if model == GenreTitle:
+                        objects.append(GenreTitle(
+                            id=row['id'],
+                            title_id=row['title_id'],
+                            genre_id=row['genre_id']
+                        ))
+                    if model == Review:
+                        objects.append(Review(
+                            id=row['id'],
+                            title_id=row['title_id'],
+                            text=row['text'],
+                            author=User.objects.get(id=row['author']),
+                            score=row['score'],
+                            pub_date=row['pub_date']
+                        ))
+                    if model == User:
+                        objects.append(User(
+                            id=row['id'],
+                            username=row['username'],
+                            email=row['email'],
+                            role=row['role'],
+                            bio=row['bio'],
+                            first_name=row['first_name'],
+                            last_name=row['last_name']
+                        ))
+
+                model.objects.bulk_create(objects, ignore_conflicts=True)
+                self.stdout.write(self.style.SUCCESS(f'Данные из {file} успешно загружены.'))
+
+        self.stdout.write(self.style.SUCCESS('Загрузка данных завершена успешно.'))
