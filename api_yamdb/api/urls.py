905a3fbbf57dcaf70c0a4d45a8dae8206cddf5b0
Alexander Boyko <sasha.boyko.dg.19@gmail.com>
2025-01-26 15:31:38 +0300

    оставил все как есть

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,7 +2,6 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from users.views import UserViewSet
-
 from .views import (
     CategoryViewSet,
     CommentViewSet,
