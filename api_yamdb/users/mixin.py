f824f8583bc81dc1c97cc1a86f72f72bc017a028
Alexander Boyko <sasha.boyko.dg.19@gmail.com>
2025-02-04 15:05:31 +0300

    перенес валидацию в миксин

--- api_yamdb/users/mixin.py
+++ api_yamdb/users/mixin.py
@@ -1,9 +1,19 @@
+from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
 
 class UsernameValidationMixin:
+    username_validator = RegexValidator(
+        regex=r"^[\w.@+_-]+\Z",
+        message="Можно использовать только буквы "
+                "(включая буквы в верхнем и нижнем регистрах), "
+                "цифры и спецсимволы: ., @, +, - ",
+        code="invalid_username",
+    )
+
     def validate_username(self, username):
+        self.username_validator(username)
         if username.lower() == 'me':
             raise serializers.ValidationError(
                 _('Пользователь с таким именем уже существует'),
