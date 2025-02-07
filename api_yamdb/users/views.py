02f0e5362223e4a9460ea7a14c45b344d1c82a20
Alexander Boyko <sasha.boyko.dg.19@gmail.com>
2025-02-07 15:02:40 +0300

    изменил скобки и кавычки, добавил константу из модели

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -12,6 +12,7 @@ from rest_framework.decorators import (
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from .models import Role
 from api.permissions import IsAdminUserOrSuperuser
 from users.models import User
 from users.serializers import (
@@ -42,14 +43,14 @@ class UserViewSet(viewsets.ModelViewSet):
     # Для выбора сериализатора в зависимости от роли
     def get_serializer_class(self):
         if (self.request.user.is_authenticated
-                and self.request.user.role == "admin"):
+                and self.request.user.role == Role.ADMIN):
             return UserSerializer
         return UserNoAdminSerializer
 
     @action(
         detail=False,
         url_path="me",
-        permission_classes=[IsAuthenticated]
+        permission_classes=(IsAuthenticated,)
     )
     def me(self, request):
         user = request.user
@@ -72,5 +73,5 @@ class UserViewSet(viewsets.ModelViewSet):
 def get_token_obtain_pair_view(request):
     serializer = UserTokenSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
-    token = serializer.validated_data['token']
+    token = serializer.validated_data["token"]
     return Response({"token": token}, status=status.HTTP_200_OK)
